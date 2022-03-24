0f28a8dc619699e6a47e351e2f4be12debc97f58
ThalesLoreto <tloreto.dev@gmail.com>
2022-03-23 23:49:42 -0300

    add prisma mode with lsp

--- packages.el
+++ packages.el
@@ -16,6 +16,8 @@
 (package! handlebars-mode)
 ;; Discord rich presence
 (package! elcord)
+;; Prisma mode, syntax, autocomplete, etc...
+(package! prisma-mode :recipe (:host github :repo "pimeys/emacs-prisma-mode" :branch "main"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
